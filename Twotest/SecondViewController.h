0b90b6dada9f6a756bc0e7ec7dbae8b48cefea59
Beauty1124 <zhangpping1124@163.com>
2015-11-23 22:18:02 +0800

    uuuuu

---
+++ Twotest/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Twotest
+//
+//  Created by Beauty on 15/11/23.
+//  Copyright © 2015年 Beauty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
